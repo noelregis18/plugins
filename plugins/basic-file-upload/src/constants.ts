eb3faa8901dc063a61aee9b2c20a71157ffe8536
Kartik Jolapara <kjmickey003@gmail.com>
2023-11-15 23:15:55 +0530

    feat: Add dependencies to constants.ts
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- plugins/basic-file-upload/src/constants.ts
+++ plugins/basic-file-upload/src/constants.ts
@@ -10,5 +10,27 @@ export const envVariables: VariableDictionary = [
   { SERVER_URL: "http://localhost:3000" },
 ];
 
+export const adminUIPackageJsonValues = {
+  dependencies: {
+    "@auth0/auth0-spa-js": "^2.1.2",
+    "react-router-dom": "^5.3.3",
+    history: "4.10.1",
+  },
+  devDependencies: {
+    "@types/react-router-dom": "5.3.2",
+    "@types/history": "^4.7.11",
+  },
+};
+
+export const serverPackageJsonValues = {
+  dependencies: {
+    "graphql-upload": "^13.0.0",
+  },
+  devDependencies: {
+    "@types/graphql-upload": "^8.0.12",
+    "@types/multer": "^1.4.8",
+  },
+};
+
 export const staticsPath = join(__dirname, "static");
 export const templatesPath = join(__dirname, "templates");
