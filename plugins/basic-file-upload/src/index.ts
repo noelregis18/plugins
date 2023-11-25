e0e11780ae5bb5a2541c849243b1434a3fe106ef
Kartik Jolapara <kjmickey003@gmail.com>
2023-11-25 22:04:00 +0530

    feat: Add FileArgs.ts to the entities - modify tsconfig to exclude template and static directories
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- plugins/basic-file-upload/src/index.ts
+++ plugins/basic-file-upload/src/index.ts
@@ -9,14 +9,24 @@ import type {
   Events,
   ModuleMap,
 } from "@amplication/code-gen-types";
+import {
+  print,
+  readFile,
+  removeTSClassDeclares,
+} from "@amplication/code-gen-utils";
 import { EnumDataType, EventNames } from "@amplication/code-gen-types";
+import { builders } from "ast-types";
 import { after, merge } from "lodash";
-import { resolve } from "path";
-import { envVariables } from "./constants";
-import fs from 'fs';
-import path from 'path';
+import { join, resolve } from "path";
+import {
+  envVariables,
+  generateInvalidFileFieldError,
+  serverPackageJsonValues,
+  templatesPath,
+} from "./constants";
+import { interpolate } from "./utils";
 
-class ExamplePlugin implements AmplicationPlugin {
+class BasicFileUploadPlugin implements AmplicationPlugin {
   /**
    * This is mandatory function that returns an object with the event name. Each event can have before or/and after
    */
@@ -34,7 +44,7 @@ class ExamplePlugin implements AmplicationPlugin {
         // after: this.afterCreateServerPackageJson,
       },
       [EventNames.CreateDTOs]: {
-        // before: this.beforeCreateDTOs,
+        before: this.beforeCreateDTOs,
         // after: this.afterCreateDTOs,
       },
     };
@@ -51,7 +61,22 @@ class ExamplePlugin implements AmplicationPlugin {
   }
 
   beforeCreateServer(context: DsgContext, eventParams: CreateServerParams) {
-this.verifyEntitiesWithFileFields(context);
+    // TODO: check if there aren't any file fields so that we can notify the user that the plugin is enabled but there are no file fields so either disable this plugin or add file fields
+    context.entities?.forEach(({ name: entityName, fields }) => {
+      const findInvalidFileType = fields.find(
+        ({ name, dataType }) =>
+          name.startsWith("file") && dataType !== EnumDataType.Json,
+      );
+
+      if (findInvalidFileType) {
+        context.logger.error(
+          generateInvalidFileFieldError(entityName, findInvalidFileType.name),
+        );
+        throw new Error(
+          generateInvalidFileFieldError(entityName, findInvalidFileType.name),
+        );
+      }
+    });
     return eventParams;
   }
 
@@ -68,6 +93,56 @@ this.verifyEntitiesWithFileFields(context);
       context.serverDirectories.srcDirectory + "/util",
     );
     await modules.merge(staticsFiles);
+
+    // Independent Event
+    const { entities, serverDirectories } = context;
+
+    // A map sort of all the entities which will store all the entities that have file fields
+    const entitiesWithOnlyFileFields: { [key: string]: string[] } = {};
+    entities?.forEach(({ name: entityName, fields }) => {
+      fields.forEach(({ name: fieldName, dataType }) => {
+        if (fieldName.startsWith("file")) {
+          if (entitiesWithOnlyFileFields[entityName])
+            entitiesWithOnlyFileFields[entityName].push(fieldName);
+          else entitiesWithOnlyFileFields[entityName] = [fieldName];
+        }
+      });
+    });
+
+    if (Object.keys(entitiesWithOnlyFileFields).length > 0) {
+      Object.keys(entitiesWithOnlyFileFields).forEach(async (entityName) => {
+        context.logger.info(`Creating ${entityName}FileArgs.ts file...`);
+        const entityNameToLower = entityName.toLowerCase();
+        const template = await readFile(
+          join(templatesPath, "entityFileArgs.template.ts"),
+        );
+        // const entityFileArgsImport = importNames(
+        //   [builders.identifier(entityName)],
+        //   `../${entityNameToLower}/${entityNameToLower}.entity`,
+        // );
+        // const entityFileArgsImport =
+
+        interpolate(template, {
+          ENTITY_FILES_ARRAY: builders.arrayExpression(
+            entitiesWithOnlyFileFields[entityName].map((fieldName) =>
+              builders.stringLiteral(fieldName),
+            ),
+          ),
+          ENTITY_FILES: builders.identifier(`${entityNameToLower}Files`),
+          ENTITY_FILES_TYPE: builders.identifier(
+            `${entityNameToLower}FilesTypes`,
+          ),
+        });
+
+        modules.set({
+          code: print(template).code,
+          path: `${context.serverDirectories.srcDirectory}/${entityNameToLower}/base/${entityName}FileArgs.ts`,
+        });
+
+        context.logger.info(`Created ${entityName}FileArgs.ts file...`);
+      });
+    }
+
     return modules; // You must return the generated modules you want to generate at this part of the build.
   }
 
@@ -75,50 +150,9 @@ this.verifyEntitiesWithFileFields(context);
     context: DsgContext,
     eventParams: CreateServerPackageJsonParams,
   ) {
-    const myValues = {
-      dependencies: {
-        "graphql-upload": "^13.0.0",
-      },
-      devDependencies: {
-        "@types/graphql-upload": "^8.0.12",
-        "@types/multer": "^1.4.8",
-      },
-    };
-
-    eventParams.updateProperties.forEach((updateProperty) =>
-      merge(updateProperty, myValues),
-    );
+    eventParams.updateProperties.push(serverPackageJsonValues);
 
     return eventParams;
-  }
-
-  verifyEntitiesWithFileFields(context: DsgContext) {
-    return context.entities?.filter((entity) =>
-      entity.fields.some((field) => field.name.startsWith("file")),
-    );
-  }
-
-  findEntityFileFields(context: DsgContext) {
-    const generateErrorMessage = (entityName: string, fieldName: string) =>
-    `DataType of field: ${fieldName} should be JSON on entity: ${entityName}. As you've prefixed it with "file..." it should be a JSON field to store the file metadata else you can use any other prefix.`;
-
-  context.entities?.forEach(({ name: entityName, fields }) => {
-    debugger;
-
-    const findInvalidFileType = fields.find(
-      ({ name, dataType }) =>
-        name.startsWith("file") && dataType !== EnumDataType.Json,
-    );
-
-    if (findInvalidFileType) {
-      context.logger.error(
-        generateErrorMessage(entityName, findInvalidFileType.name),
-      );
-      throw new Error(
-        generateErrorMessage(entityName, findInvalidFileType.name),
-      );
-    }
-  });
   }
 
   beforeCreateDTOs(context: DsgContext, eventParams: CreateDTOsParams) {
@@ -128,27 +162,38 @@ this.verifyEntitiesWithFileFields(context);
     // find the entity that has file field
     // add the file field to the dto
 
-    context.logger.warn("beforeCreateDTOs", { dtos: dtos[0] });
-    
+    // context.logger.warn("beforeCreateDTOs", { dtos: dtos[0] });
+
+    // context.logger.warn("olleh", {
+    //   updateARgs: eventParams.dtos["User"],
+    //   updateInput: eventParams.dtos["User"],
+    // });
+    context.logger.warn("beforeCreateDTOs", {
+      // createInput: eventParams.dtos["User"].createInput,
+      createInputArgs: eventParams.dtos["User"].createArgs,
+    });
+
     return eventParams;
   }
 
-  // async afterCreateDTOs(
-  //   context: DsgContext,
-  //   eventParams: CreateDTOsParams,
-  //   modules: ModuleMap,
-  // ) {
+  async afterCreateDTOs(
+    context: DsgContext,
+    eventParams: CreateDTOsParams,
+    modules: ModuleMap,
+  ) {
+    // context.logger.warn("olleh", {
+    //   updateARgs: eventParams.dtos["User"],
+    //   updateInput: eventParams.dtos["User"],
+    // });
 
-  //   context.entities?.forEach(({ name: entityName, fields }) => {
+    // context.entities?.forEach(({ name: entityName, fields }) => {
 
-  //     context.logger.warn('afterCreateDTOs', {DTO: eventParams.dtos[entityName].updateInput.})
+    //   context.logger.warn('afterCreateDTOs', {DTO: eventParams.dtos[entityName].updateInput.})
 
-
-      
-  //   }
-  //   );
-  //   return modules;
-  // }
+    // }
+    // );
+    return modules;
+  }
 }
 
-export default ExamplePlugin;
+export default BasicFileUploadPlugin;
--- plugins/basic-file-upload/src/utils/helpers.ts
+++ plugins/basic-file-upload/src/utils/helpers.ts
@@ -1,6 +1,6 @@
 import { DsgContext, EnumDataType } from "@amplication/code-gen-types";
 
-export const verifyEntitiesWithFileFields = (context: DsgContext) => {
+export const findEntitiesWithFileFields = (context: DsgContext) => {
   return context.entities?.filter((entity) =>
     entity.fields.some((field) => field.name.startsWith("file")),
   );
